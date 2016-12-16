5f4f9d8ca6afec680679e1a17ea8421ac5128b8c
Emmanuel Frecon <efrecon@gmail.com>
2016-12-16 16:07:26 +0100

    Adding tests

---
+++ tests/all.tcl
@@ -0,0 +1,10 @@
+package prefer latest
+package require Tcl
+package require tcltest 2.2
+namespace import tcltest::*
+configure {*}$argv -testdir [file dir [info script]]
+if {[singleProcess]} {
+    interp debug {} -frame 1
+}
+runAllTests
+proc exit args {}
