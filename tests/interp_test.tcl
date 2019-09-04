ec818b15dad942186c8b04d51277dcb0c5196fe4
Emmanuel Frecon <efrecon@gmail.com>
2019-09-04 22:53:40 +0200

    GET tests

--- tests/interp_test.tcl
+++ tests/interp_test.tcl
@@ -1,3 +1,11 @@
 proc http_version {} {
     return [package require http]
 }
+
+proc http_get { url } {
+    package require http
+    set tok [::http::geturl $url]
+    set ncode [::http::ncode $tok]
+    ::http::cleanup $tok
+    return $ncode
+}
